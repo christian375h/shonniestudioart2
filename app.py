8051c0c7e091d9329a090fce1b4f53fe90204f01
Christian Hess <christian375h@gmail.com>
2024-03-15 21:15:19 -0600

    ssl config
    changed port to ssl

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ def classes():
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'christian375h@gmail.com'
 app.config['MAIL_PASSWORD'] = mail_password
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route('/form', methods=["POST"])
