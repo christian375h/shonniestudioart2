0cbd3441e8258459b7cfa0e57391d74d9bb13f72
Christian Hess <christian375h@gmail.com>
2024-03-19 22:35:45 -0600

    Classes Page
    Finally started adding content to the classes page

--- static/script.js
+++ static/script.js
@@ -5,3 +5,17 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function openLink(tabName){
+    var projectLinks = document.getElementsByClassName("class-links");
+    var projectTabs = document.getElementsByClassName("class-tabs");
+
+    for(projectLink of projectLinks){
+        projectLink.classList.remove("active-class-link");
+    }
+    for(projectTab of projectTabs){
+        projectTab.classList.remove("active-class-tab");
+    }
+    event.currentTarget.classList.add("active-class-link");
+    document.getElementById(tabName).classList.add("active-class-tab");
+}
